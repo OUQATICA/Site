2b2cc92eaeba970f263fec6d22f1148bd9802c8e
OUQATIKA <101516507+OUQATICA@users.noreply.github.com>
2024-11-08 13:28:52 +0700

    lr-3

--- scripts/calc_second_function.js
+++ scripts/calc_second_function.js
@@ -10,22 +10,22 @@ function calcValue(event) {
     switch (y) {
         case 3:
             result = b - y**2 - 1;
-            func = `${b} - x² - 1`;
+            func = `${b} - x² - 1 = `;
             break;
         case 8:
             result = Math.sqrt(Math.log(y) + a);
-            func = `√(ln(x) + ${a})`;
+            func = `√(ln(x) + ${a}) = `;
             break;
         case 7:
             result = Math.cos((a*y**2) + 3)**2;
-            func = `cos(${a} ∙ x²) + 3`;
+            func = ` =cos(${a} ∙ x²) + 3 = `;
             break;
         default:
-            result = 'ass';
-            func = 'ass'
+            result = 'Ошибка: значение x не поддерживается';
+            func = '';
     }
     document.getElementById('resulSecondFunction').innerHTML = `
-            <p class="text">${func} = ${result}</p>
+            <p class="text">${func}${result}</p>
         `;
 
 }
